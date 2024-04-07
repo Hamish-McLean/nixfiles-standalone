379fad74822bc712d85101585383bcb1a003e354
Hamish McLean <HamishMcLean94@gmail.com>
2024-04-07 11:22:57 +0100

    Created nixberry config

---
+++ hosts/NixBerry/default.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, unstablePkgs, lib, nixos-hardware, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    nixos-hardware.nixosModules.raspberry-pi-4
+    ../common.nix
+  ];
+
+  # Bootloader
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    loader = {
+      grub.enable = false;
+      generic-extlinux.compatible.enable = true; # What does this do?
+    };
+  };
+
+  networking = {
+    hostName = "NixBerry";
+    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true;
+  };
+
+  programs.fish.enable = true;
+
+  services.openssh.enable = true;
+
+  services.tailscale = true;
+
+  virtualisation.docker.enable = true; # Docker running as root
+
+  users.users.cycad = {
+    isNormalUser = true;
+    description = "Hamish McLean";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "23.11";
+}
