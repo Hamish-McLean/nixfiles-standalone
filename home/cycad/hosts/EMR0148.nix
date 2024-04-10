ed4f86865ef4491a49bdce2d82bbb65340b674f0
Hamish McLean <HamishMcLean94@gmail.com>
2024-04-10 19:04:42 +0100

    Added WSL hosts to home-manager

---
+++ home/cycad/hosts/EMR0148.nix
@@ -0,0 +1,20 @@
+/*
+Cycad's home manager module for EMR0148.
+This imports Cycad's default home manager module which imports the other modules.
+Modules can be enabled or disabled here.
+*/
+{ ... }:
+{
+  imports = [
+    ../default.nix
+  ];
+
+  # Enable all cliPrograms modules
+  cliPrograms.enable = true;
+
+  # Enable all guiPrograms modules
+  guiPrograms.enable = false;
+
+  # Disable specific modules
+
+}
