9e6b367405045ea0a752481aae16801c9a524eb6
Hamish McLean <HamishMcLean94@gmail.com>
2025-01-21 18:13:58 +0000

    Added settings for nix-on-droid

--- hosts/nix-on-droid/default.nix
+++ hosts/nix-on-droid/default.nix
@@ -3,9 +3,9 @@
   ...
 }:
 {
-  # imports = [
-  #   ../common.nix
-  # ];
+  imports = [
+    #../../modules/catppuccin.nix
+  ];
 
   environment.packages = with pkgs; [
     # fish
@@ -15,6 +15,24 @@
     nano
     nh
   ];
+
+  #cat-colours.enable = true;
+
+  terminal = {
+    font = ~/.local/state/nix/profiles/home-manager/home-path/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFont-Regular.ttf;
+    colors = {
+      background = "#1e1e2e";
+      foreground = "#cdd6f4"; # Main text colour
+      cursor = "#cdd6f4";
+    };
+  };
+
+  android-integration = {
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+  }; # There are a few more integrations
 
   # user.shell = pkgs.fish;
 
